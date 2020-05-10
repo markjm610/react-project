d12bc28eb8d112b9c471d43a0d7a94a9bd4e6fd3
Mark Mansolino <markjm610@gmail.com>
2020-05-10 12:37:50 -0400

    Add AuthRoute and ProtectedRoute files

---
+++ front-end/react-project/src/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavBar = () => {
+
+    // map over projects for user from fetch request to make tabs to display projects
+}
+
+export default NavBar;
