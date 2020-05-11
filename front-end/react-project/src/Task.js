364bf4b40675f1cae6ec4b1d698b90be90d80499
Mark Mansolino <markmansolino@Marks-MacBook-Air.local>
2020-05-11 13:51:16 -0400

    Add to layout of home

---
+++ front-end/react-project/src/Task.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Task = () => {
+    return (
+        <>
+
+            <div className='task-drop-zone'>
+                <div className='task'>
+                    <div className='task__heading'>Task heading</div>
+                    <div className='task__description'>Task description will be here</div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Task;
