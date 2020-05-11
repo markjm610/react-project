c12683636e684d1af8f11975f6b270f4e3a7629c
Mark Mansolino <markjm610@gmail.com>
2020-05-11 15:15:36 -0400

    Add NavBar in Home

--- front-end/react-project/src/Home.js
+++ front-end/react-project/src/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './Home.css';
-import Column from './Column';
+import NavBar from './NavBar';
+import WorkingArea from './WorkingArea';
+import { Route } from 'react-router-dom';
 
-const columnArray = [0, 1]
+const projectArr = [1, 2, 3]
 
 const Home = () => {
 
     return (
         <>
-            <div className='sidebar-left'></div>
-            <div className='working-area'>{columnArray.map(column => <Column></Column>)}</div>
+            <div className='sidebar-left'>
+                <NavBar projectArr={projectArr}></NavBar>
+            </div>
+            <div className='working-area'>
+                <Route exact path='/home/project/1' component={WorkingArea}></Route>
+            </div>
             <div className='sidebar-right'></div>
         </>
     )
