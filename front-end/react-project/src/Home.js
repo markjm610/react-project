edd75bb153c440e044cab92edbe0ca87dc252e87
Mark Mansolino <markmansolino@Marks-MacBook-Air.local>
2020-05-11 12:37:00 -0400

    Begin home page

---
+++ front-end/react-project/src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Home.css';
+
+const Home = () => {
+
+    return (<>
+        <div className='column-1'></div>
+        <div className='column-2'></div>
+        <div className='column-3'></div>
+        <div className='column-4'></div>
+        <div className='column-5'></div>
+    </>
+    )
+
+}
+
+export default Home;
