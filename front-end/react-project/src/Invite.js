2f13c25505d23dd666975a10bf178276f765e492
Mark Mansolino <markjm610@gmail.com>
2020-05-14 00:56:36 -0400

    Invite

--- front-end/react-project/src/Invite.js
+++ front-end/react-project/src/Invite.js
@@ -10,44 +10,47 @@ const Invite = () => {
     const [value, setValue] = useState({ name: '' });
     const [showConfirm, setShowConfirm] = useState()
 
-    // const submitInvite = async () => {
-    //     const findRes = await fetch(`${apiBaseUrl}/users`, {
-    //         method: 'PUT',
-    //         body: JSON.stringify(value),
-    //         headers: {
-    //             "Content-Type": 'application/json',
-    //         }
-    //     })
+    const submitInvite = async () => {
+        console.log(value)
+        const findRes = await fetch(`${apiBaseUrl}/users`, {
+            method: 'PUT',
+            body: JSON.stringify(value),
+            headers: {
+                "Content-Type": 'application/json',
+            }
+        })
 
 
-    //     // // Probably should send back and display an error message if person is not found
+        // // Probably should send back and display an error message if person is not found
 
-    //     // // if (findRes.ok) maybe
+        // // if (findRes.ok) maybe
 
-    //     const parsedFindRes = await findRes.json();
-    //     const inviteId = parsedFindRes.user.id;
-    //     const userId = currentUserId;
-    //     const projectId = currentProjectId;
-    //     const sendInviteRes = await fetch(`${apiBaseUrl}/users/invites`, {
-    //         method: 'PUT',
-    //         body: JSON.stringify({ inviteReceiver: inviteId, inviteSender: userId, projectId }),
-    //         headers: {
-    //             "Content-Type": 'application/json',
-    //         }
-    //     })
+        const parsedFindRes = await findRes.json();
+        const inviteId = parsedFindRes.user.id;
+        const userId = currentUserId;
+        const projectId = currentProjectId;
+        console.log(parsedFindRes);
+        console.log(userId);
+        const sendInviteRes = await fetch(`${apiBaseUrl}/users/invites`, {
+            method: 'PUT',
+            body: JSON.stringify({ inviteReceiver: inviteId, inviteSender: userId, projectId }),
+            headers: {
+                "Content-Type": 'application/json',
+            }
+        })
 
-    //     if (sendInviteRes.ok) {
-    //         console.log('invite sent')
-    //     }
+        if (sendInviteRes.ok) {
+            console.log('invite sent')
+        }
 
-    //     // Have to check for invites in database when the page loads
+        // Have to check for invites in database when the page loads
 
-    // }
+    }
 
     return (<>
-        <div className='invite'><ShareOption
+        <div className='invite'>{currentProjectId && <ShareOption
             className='invite-icon'
-            onClick={() => setShow(true)}></ShareOption></div>
+            onClick={() => setShow(true)}></ShareOption>}</div>
         {show && (
             <Layer
                 onEsc={() => setShow(false)}
@@ -57,7 +60,7 @@ const Invite = () => {
                     value={value}
                     onChange={nextValue => setValue(nextValue)}
                     onReset={() => setValue({})}
-                // onSubmit={submitInvite}
+                    onSubmit={submitInvite}
                 >
                     <FormField name="name" htmlfor="text-input-id" label="Who would you like to invite?">
                         <TextInput id="text-input-id" name="name" />
