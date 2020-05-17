49053a6fb2ccd67fc8ef8211b9b2f664c56571b0
Mark Mansolino <markmansolino@Marks-MacBook-Air.local>
2020-05-16 23:41:02 -0400

    minor changes

--- back-end/routes/tasks.js
+++ back-end/routes/tasks.js
@@ -37,13 +37,15 @@ router.delete('/tasks/:taskId', asyncHandler(async (req, res) => {
 
 }))
 
-router.patch('/tasks', asyncHandler(async (req, res) => {
+router.put('/tasks', asyncHandler(async (req, res) => {
     try {
         const { sendArr } = req.body;
         sendArr.forEach(async (task) => {
             console.log('column position', task.columnPosition);
             console.log('columnId', task.columnId);
-            await Task.update({ columnPosition: task.columnPosition, columnId: task.columnId }, { where: { id: task.id } });
+            await Task.update(
+                { columnPosition: task.columnPosition, columnId: task.columnId },
+                { where: { id: task.id } });
         })
 
     } catch (e) {
