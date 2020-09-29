7444e184d42f53bba7f7b271b459b5562233eaa7
Mark Mansolino <markjm610@gmail.com>
2020-09-28 20:34:28 -0400

    Adding tests

---
+++ front-end/react-project/src/__tests__/ControlSpeed.test.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+// import ControlSpeed from '../ControlSpeed';
+
+// test('clicked changes to slow, medium, or fast based on button clicked', () => {
+//     const component = renderer.create(
+//         <ControlSpeed />
+//     )
+
+//     let tree = component.toJSON()
+//     expect(tree).toMatchSnapshot()
+
+
+
+// })
