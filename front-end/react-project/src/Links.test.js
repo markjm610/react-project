3767a0177101c1ce98a4a6b9376d9235b592686c
Mark Mansolino <markjm610@gmail.com>
2020-09-28 21:20:59 -0400

    Add Links test

---
+++ front-end/react-project/src/Links.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, mount, render, configure } from 'enzyme';
+import Links from './Links'
+import { Github, Linkedin } from 'grommet-icons'
+configure({ adapter: new Adapter() });
+
+it('renders without crashing', () => {
+    shallow(<Links />)
+})
+
+it('links to my GitHub with GitHub icon', () => {
+    const wrapper = shallow(<Links />)
+    const githubLink = <a href='https://github.com/markjm610'>
+        <Github color='steelblue' style={{ marginRight: '10px' }} />
+    </a>
+    expect(wrapper.contains(githubLink)).toEqual(true)
+})
