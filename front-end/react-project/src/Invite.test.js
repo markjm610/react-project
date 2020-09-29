f1eac7686219bc7b076f5488f0afe79805cd33a7
Mark Mansolino <markjm610@gmail.com>
2020-09-29 18:04:39 -0400

    Invite tests

--- front-end/react-project/src/Invite.test.js
+++ front-end/react-project/src/Invite.test.js
@@ -23,10 +23,43 @@ it('shows the layer when the icon is clicked', () => {
     expect(wrapper.find('Layer').length).toBe(1)
 })
 
-// it('sets different invite statuses based on response after submitting invite', () => {
+it('is disabled while alphabetizing', () => {
+    const context = {
+        currentProjectId: 1,
+        alphabetizing: true
+    }
+    const wrapper = mount(<Context.Provider value={context}><Invite /></Context.Provider>)
+    const icon = wrapper.find('ShareOption')
+    icon.simulate('click')
+    expect(wrapper.find('Layer').length).toBe(0)
+})
 
-// })
+
+it('displays different invite statuses based on response after submitting invite', () => {
+    const context = {
+        currentProjectId: 1,
+        alphabetizing: false
+    }
+    const wrapper = mount(<Context.Provider value={context}><Invite /></Context.Provider>)
+    const invite = wrapper.find('Invite')
+    // mimic response
+    // test jsx changes
+})
 
 // it('displays different things based on invite status')
 
-// it('resets form input value and invite status to empty string when closing the layer')
+// it('resets form input value and invite status to empty string when closing the layer', () => {
+//     const context = {
+//         currentProjectId: 1,
+//         alphabetizing: false
+//     }
+//     const wrapper = mount(<Context.Provider value={context}><Invite /></Context.Provider>)
+//     const icon = wrapper.find('ShareOption')
+//     icon.simulate('click')
+//     const layer = wrapper.find('Layer')
+//     expect(layer.length).toBe(1)
+//     layer.prop('onClickOutside')
+//     const layerAfter = wrapper.find('Layer')
+//     expect(layerAfter.length).toBe(0)
+//     expect()
+// })
