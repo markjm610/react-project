3aac54c2f9a82a001520edafa963f35d23b21c48
Mark Mansolino <markjm610@gmail.com>
2020-09-30 23:26:01 -0400

    Invite tests

--- front-end/react-project/src/Invite.test.js
+++ front-end/react-project/src/Invite.test.js
@@ -5,6 +5,11 @@ import Invite from './Invite';
 import Context from './Context'
 import { Layer } from 'grommet';
 
+beforeEach(() => {
+    fetch.resetMocks();
+});
+
+
 it('renders with context', () => {
     const context = {
         currentProjectId: 1,
@@ -51,16 +56,34 @@ it('opens the layer on click and closes the layer on click outside', () => {
     expect(newLayer.length).toBe(0)
 })
 
-it('displays different invite statuses based on inviteStatus hook', () => {
-    const context = {
-        currentProjectId: 1,
-        alphabetizing: false
-    }
-    const wrapper = mount(<Context.Provider value={context}><Invite /></Context.Provider>)
-    const icon = wrapper.find('ShareOption')
-    icon.simulate('click')
-    const popupContainer = wrapper.find('.popup-container')
-    expect(popupContainer.contains(<div className='popup-text'>Who would you like to invite?</div>)).toBe(true)
+// it('displays different invite statuses based on response from submitting an invite', () => {
+//     const context = {
+//         currentProjectId: 1,
+//         alphabetizing: false
+//     }
+//     const wrapper = mount(<Context.Provider value={context}><Invite /></Context.Provider>)
+//     const icon = wrapper.find('ShareOption')
+//     icon.simulate('click')
+//     const popupContainer = wrapper.find('.popup-container')
+//     expect(popupContainer.contains(<div className='popup-text'>Who would you like to invite?</div>)).toBe(true)
+//     expect(popupContainer.contains(<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '150px', marginTop: '10px' }}>User not found</div>)).toBe(false)
+
+//     // Mock response from fetch
+//     fetch.mockResponseOnce(JSON.stringify({ message: 'already in project' }))
+//     // fetch.mockResponseOnce(JSON.stringify({ message: 'sent' }))
+//     // Simulate form submit
+//     // const form = popupContainer.find('form')
+//     const input = popupContainer.find('input')
+
+//     input.instance().value = 'User'
+//     expect(input.instance().value).toBe('User')
+//     // form.simulate('submit')
+//     const button = popupContainer.find('.popup-button')
+//     button.simulate('submit')
+//     const newWrapper = mount(<Context.Provider value={context}><Invite /></Context.Provider>)
+//     // expect(newWrapper.contains(<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '150px', marginTop: '10px' }}>User not found</div>)).toBe(true)
+//     // expect(input.instance().value).toBe('User')
+//     expect(newWrapper.contains(<div>User is already a member of this project.</div>)).toBe(true)
 })
 
 // it('clears input value and invite status on click outside', () => {
--- front-end/react-project/src/setupTests.js
+++ front-end/react-project/src/setupTests.js
@@ -1,4 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import fetchMock from "jest-fetch-mock";
 
+fetchMock.enableMocks();
 configure({ adapter: new Adapter() });
