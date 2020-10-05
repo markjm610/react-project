9bdc284c17b9af803624391a455333cdbb0dfebe
Mark Mansolino <markjm610@gmail.com>
2020-10-04 22:35:46 -0400

    Remove a console log

--- front-end/react-project/src/Home.js
+++ front-end/react-project/src/Home.js
@@ -327,7 +327,7 @@ const Home = ({ history }) => {
 
         }
         else if (type === 'project') {
-            console.log(result)
+            // console.log(result)
             // if (destination.droppableId === 'project-nav-list' && source.droppableId === 'project-nav-main') {
             //     return
             // }
